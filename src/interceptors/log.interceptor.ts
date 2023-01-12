0c4688a8996d58e4d947b27180c883b89815c75e
Yuri Silva <yurikgs@outlook.com>
2023-01-12 13:51:17 -0300

    Criado interceptador de descrição de rota/ tempo de execução, com logs - LogInterceptor. Em comentários, as chamadas  de execução do middleware na main, no controller de user e em método do user controller, como exemplos de implementação

---
+++ src/interceptors/log.interceptor.ts
@@ -0,0 +1,29 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+export class LogInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const dt = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const request = context.switchToHttp().getRequest();
+        console.log(`URL: ${request.url}`);
+        console.log(`METHOD: ${request.method}`);
+        console.log(`Execução em ${Date.now() - dt} milissegundos`);
+      }),
+    );
+  }
+}
+
+/**
+ * RxJS tap() operator is a utility operator that returns an observable output that is identical to the source observable but performs a side effect for every emission on the source observable.
+
+In other words, you can say that the RxJS tap() operator is used to intercept each emission on the source observable and runs a function but returns an output that is identical to the source observable as long as it doesn't find any error.
+
+This operator is generally used for debugging observables for the correct values or performing other side effects.
+ * 
+ */
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,14 +8,17 @@ import {
   Patch,
   Put,
   ParseIntPipe,
+  UseInterceptors,
   BadRequestException,
   Logger,
 } from '@nestjs/common';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UserService } from './user.service';
 
+// @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -27,6 +30,7 @@ export class UserController {
   async list() {
     return this.userService.list();
   }
+  // @UseInterceptors(LogInterceptor)
   @Get(':id')
   async show(@Param('id', ParseIntPipe) id: number) {
     return this.userService.show(id);
